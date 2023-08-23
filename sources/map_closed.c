58bbf38aabe56acd402a276892c9369069646eb7
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-08-23 17:36:53 +0100

    i thinks m done with parsing

--- sources/map_closed.c
+++ sources/map_closed.c
@@ -6,15 +6,15 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 15:34:53 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/08/23 16:34:29 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/08/23 17:36:16 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-static int	check_zero_with_space(char *s, int *index)
+static int	check_zero_with_space(char *s, size_t *index)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (s[i])
@@ -29,7 +29,7 @@ static int	check_zero_with_space(char *s, int *index)
 	return (0);
 }
 
-static int	check_index_with_space(char *s, int index)
+static int	check_index_with_space(char *s, size_t index)
 {
 	if (s[index])
 	{
@@ -41,10 +41,10 @@ static int	check_index_with_space(char *s, int index)
 
 int	map_closed(t_game *game)
 {
-	char	*current;
-	char	*next;
-	int		i;
-	int		index;
+	char		*current;
+	char		*next;
+	int			i;
+	size_t		index;
 
 	i = 0;
 	while (game->map[i])
@@ -55,7 +55,8 @@ int	map_closed(t_game *game)
 			if (game->map[i + 1])
 			{
 				next = game->map[i + 1];
-				if (check_index_with_space(next, index))
+				if (check_index_with_space(next, index)
+					|| index >= ft_strlen(next))
 				{
 					ft_putendl_fd("Error: Map Is Not Surrounded By Walls", 2);
 					return (1);
