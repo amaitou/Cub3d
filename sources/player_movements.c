2390323090b048f2c7de20119077d4f83721c258
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-09-13 23:34:30 +0100

    almost handled player angle

--- sources/player_movements.c
+++ sources/player_movements.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 17:51:09 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/09/13 19:26:25 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/09/13 23:32:36 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ void	move_up(t_game *game)
 	float		y;
 
 	player = &game->player;
-	y = (game->player.y - player->move_speed) / TILE;
-	x = player->x / TILE;
+	y = (game->player.y + sin(game->player.rotation_angle)
+			* game->player.move_speed) / TILE;
+	x = (player->x + cos(game->player.rotation_angle)) / TILE;
 	if (game->map.map[(int)y][(int)x] != '1')
-		player->y -= player->move_speed;
+	{
+		player->x += cos(game->player.rotation_angle) * game->player.move_speed;
+		player->y += sin(game->player.rotation_angle) * game->player.move_speed;
+	}
 	return ;
 }
 
@@ -33,24 +37,14 @@ void	move_down(t_game *game)
 	float		y;
 
 	player = &game->player;
-	y = (game->player.y + player->move_speed) / TILE;
-	x = player->x / TILE;
+	y = (game->player.y - sin(game->player.rotation_angle)
+			* game->player.move_speed) / TILE;
+	x = (player->x - cos(game->player.rotation_angle)) / TILE;
 	if (game->map.map[(int)y][(int)x] != '1')
-		player->y += player->move_speed;
-	return ;
-}
-
-void	move_right(t_game *game)
-{
-	t_player	*player;
-	float		x;
-	float		y;
-
-	player = &game->player;
-	y = player->y / TILE;
-	x = (player->x + player->move_speed) / TILE;
-	if (game->map.map[(int)y][(int)x] != '1')
-		player->x += player->move_speed;
+	{
+		player->x -= cos(game->player.rotation_angle) * game->player.move_speed;
+		player->y -= sin(game->player.rotation_angle) * game->player.move_speed;
+	}
 	return ;
 }
 
@@ -61,9 +55,35 @@ void	move_left(t_game *game)
 	float		y;
 
 	player = &game->player;
-	y = player->y / TILE;
-	x = (player->x - player->move_speed) / TILE;
+	y = (player->y + sin(game->player.rotation_angle)) / TILE;
+	x = (game->player.x + cos(game->player.rotation_angle)
+			* game->player.move_speed) / TILE;
 	if (game->map.map[(int)y][(int)x] != '1')
-		player->x -= player->move_speed;
+	{
+		player->x += cos(game->player.rotation_angle - M_PI / 2)
+			* game->player.move_speed;
+		player->y += sin(game->player.rotation_angle - M_PI / 2)
+			* game->player.move_speed;
+	}
+	return ;
+}
+
+void	move_right(t_game *game)
+{
+	t_player	*player;
+	float		x;
+	float		y;
+
+	player = &game->player;
+	y = (player->y - sin(game->player.rotation_angle)) / TILE;
+	x = (game->player.x - cos(game->player.rotation_angle)
+			* game->player.move_speed) / TILE;
+	if (game->map.map[(int)y][(int)x] != '1')
+	{
+		player->x -= cos(game->player.rotation_angle - M_PI / 2)
+			* game->player.move_speed;
+		player->y -= sin(game->player.rotation_angle - M_PI / 2)
+			* game->player.move_speed;
+	}
 	return ;
 }
