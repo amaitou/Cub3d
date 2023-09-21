ddbfb808c70f3baf2bd8179989e5841f878172fc
amaitou <amait-ou@student.1337.ma>
2023-09-21 23:44:13 +0100

    deleted the python file which were for testing

--- test_valid_maps.py
+++
@@ -1,7 +0,0 @@
-
-from os import system
-x = ["creepy.cub", "hole.cub", "library.cub", "maze.cub", "random.cub", "square.cub", "subject.cub", "works.cub"]
-for i in x:
-    print(f"[*]{i}")
-    system(f"./cub3D ./maps/valid_maps/{i}")
-
