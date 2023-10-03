85753f9c976ea8def3465eb27cc20deb1a232f19
amaitou <amait-ou@student.1337.ma>
2023-10-03 04:50:37 +0100

    added some python tests for my valid & nvalid maps

---
+++ valid_maps.py
@@ -0,0 +1,4 @@
+from os import system
+invalid_maps = ["./maps/valid_maps/chaotic.cub","./maps/valid_maps/maze.cub","./maps/valid_maps/eagle.cub","./maps/valid_maps/works.cub","./maps/valid_maps/library.cub","./maps/valid_maps/subject.cub","./maps/valid_maps/random.cub","./maps/valid_maps/whitespaces.cub","./maps/valid_maps/enchanted_passage.cub","./maps/valid_maps/square.cub","./maps/valid_maps/creepy.cub","./maps/valid_maps/player_top.cub"]
+for i in invalid_maps:
+	system(f"./cub3D {i}")
