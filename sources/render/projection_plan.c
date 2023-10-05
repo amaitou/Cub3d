b464095b09bcc2d288ff2085619c32aa4120a5fa
amaitou <amait-ou@student.1337.ma>
2023-10-05 05:05:17 +0100

    added a function that renders textures

--- sources/render/projection_plan.c
+++ sources/render/projection_plan.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 15:54:57 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/10/05 04:09:17 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/10/05 05:04:06 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@ static void	draw_wall(t_game *game, int index, int x, int y)
 {
 	mlx_put_pixel(game->mlx.image, index, game->rays[index].y_start,
 		get_color_of_texture(x, y, game->vars.image));
+}
+
+static void	render_texture(t_game *game, int index)
+{
+	while (game->rays[index].y_start < game->rays[index].y_end
+		&& game->rays[index].y_start < WINDOW_HEIGHT)
+	{
+		get_texture_y(game, index, game->vars.image);
+		draw_wall(game, index, game->vars.x_texures, game->vars.y_textures);
+		game->rays[index].y_start++;
+	}
 }
 
 void	projection_plan(t_game *game)
@@ -31,13 +42,7 @@ void	projection_plan(t_game *game)
 		calculate_ending_point_of_the_wall(game, index);
 		detect_texture(game, index);
 		get_texture_x(game, index, game->vars.image);
-		while (game->rays[index].y_start < game->rays[index].y_end
-			&& game->rays[index].y_start < WINDOW_HEIGHT)
-		{
-			get_texture_y(game, index, game->vars.image);
-			draw_wall(game, index, game->vars.x_texures, game->vars.y_textures);
-			game->rays[index].y_start++;
-		}
+		render_texture(game, index);
 		++index;
 	}
 }
