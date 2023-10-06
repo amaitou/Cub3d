f89f0574f520afe32f4dad92efbdf1e99879f871
amaitou <amait-ou@student.1337.ma>
2023-10-06 13:15:31 +0100

    removed those python test files

--- invalid_maps.py
+++
@@ -1,5 +0,0 @@
-from os import system
-invalid_maps = ["./maps/invalid_maps/consecutive_nl.cub","./maps/invalid_maps/empty_file.cub","./maps/invalid_maps/missing_map.cub","./maps/invalid_maps/corners_with_zeros.cub","./maps/invalid_maps/missing_colors.cub","./maps/invalid_maps/invalid_texture_format.cub","./maps/invalid_maps/player_besides_space.cub","./maps/invalid_maps/invalid_colors.cub","./maps/invalid_maps/invalid_textures.cub","./maps/invalid_maps/rgb_range.cub","./maps/invalid_maps/wall_with_zeros.cub","./maps/invalid_maps/missing_textures.cub","./maps/invalid_maps/map_first.cub"]
-for i in invalid_maps:
-	print(f"[-] {i}")
-	system(f"./cub3D {i}")
